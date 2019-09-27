1f972199337492ebe7751449e35533c4e56ac8a5
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-09-27 16:08:15 +0300

    Add bindArr

--- library/Arrow.hs
+++ library/Arrow.hs
@@ -4,13 +4,15 @@ where
 import Arrow.Prelude
 
 
+bindArr :: ArrowApply arr => (a -> arr () b) -> arr a b
+bindArr fn = arr (\ a -> (fn a, ())) >>> app
+
 concatArr :: (ArrowApply arr, ArrowPlus arr) => arr a b -> arr [a] b
 concatArr arrow =
-  arr (\ case
-    head : tail -> ((constArr head >>> arrow) <+> (constArr tail >>> concatArr arrow), ())
-    _ -> (zeroArrow, ())
-  ) >>>
-  app
+  bindArr (\ case
+    head : tail -> (constArr head >>> arrow) <+> (constArr tail >>> concatArr arrow)
+    _ -> zeroArrow
+  )
 
 constArr :: Arrow arr => b -> arr a b
 constArr a = arr (const a)
