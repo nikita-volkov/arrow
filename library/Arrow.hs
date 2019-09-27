90b0965715d21127d6fff3301bb259d05a23a2ab
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-09-27 15:48:03 +0300

    Reimplement traversingArr without notation

--- library/Arrow.hs
+++ library/Arrow.hs
@@ -16,12 +16,14 @@ constArr :: Arrow arr => b -> arr a b
 constArr a = arr (const a)
 
 traversingArr :: ArrowChoice a => a b c -> a [b] [c]
-traversingArr f = proc list -> case list of
-  x : xs -> do
-    y <- f -< x
-    ys <- traversingArr f -< xs
-    returnA -< y : ys
-  _ -> returnA -< []
+traversingArr arrow =
+  arr (\ case
+    head : tail -> Right (head, tail)
+    _ -> Left ()
+  ) >>> (
+    constArr [] |||
+    (arrow *** traversingArr arrow >>> arr (uncurry (:)))
+  )
 
 traversingArr_ :: ArrowChoice a => a b () -> a [b] ()
 traversingArr_ f = proc list -> case list of
