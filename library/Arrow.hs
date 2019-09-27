bed66efbfd892f09a9436b7698ec9f030eb83d02
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-09-27 16:06:35 +0300

    Ditch the ings

--- library/Arrow.hs
+++ library/Arrow.hs
@@ -4,10 +4,10 @@ where
 import Arrow.Prelude
 
 
-concattingArr :: (ArrowApply arr, ArrowPlus arr) => arr a b -> arr [a] b
-concattingArr arrow =
+concatArr :: (ArrowApply arr, ArrowPlus arr) => arr a b -> arr [a] b
+concatArr arrow =
   arr (\ case
-    head : tail -> ((constArr head >>> arrow) <+> (constArr tail >>> concattingArr arrow), ())
+    head : tail -> ((constArr head >>> arrow) <+> (constArr tail >>> concatArr arrow), ())
     _ -> (zeroArrow, ())
   ) >>>
   app
@@ -19,19 +19,19 @@ constArr a = arr (const a)
 The implementation is taken from this StackOverflow answer:
 https://stackoverflow.com/a/13669572/485115
 -}
-traversingArr :: ArrowChoice a => a b c -> a [b] [c]
-traversingArr arrow =
+traverseArr :: ArrowChoice a => a b c -> a [b] [c]
+traverseArr arrow =
   arr (\ case
     head : tail -> Right (head, tail)
     _ -> Left ()
   ) >>> (
     constArr [] |||
-    (arrow *** traversingArr arrow >>> arr (uncurry (:)))
+    (arrow *** traverseArr arrow >>> arr (uncurry (:)))
   )
 
-traversingArr_ :: ArrowChoice a => a b () -> a [b] ()
-traversingArr_ f = proc list -> case list of
+traverseArr_ :: ArrowChoice a => a b () -> a [b] ()
+traverseArr_ f = proc list -> case list of
   x : xs -> do
     f -< x
-    traversingArr_ f -< xs
+    traverseArr_ f -< xs
   _ -> returnA -< ()
